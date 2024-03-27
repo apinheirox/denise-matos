e1f56d7729d4d8dc4133adc4118881a8ee06f355
Antônio Pinheiro <techwoodev@gmail.com>
2024-03-27 11:06:12 -0300

    Adicionando animações ao projeto

--- src/main.js
+++ src/main.js
@@ -22,3 +22,77 @@ document.addEventListener("DOMContentLoaded", function() {
       sidebar.classList.toggle("show");
     });
   });
+
+  $(document).ready(function(){
+    // Adicionando animação ao clicar nos links da barra de navegação
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
+  $(document).ready(function() {
+    $(window).scroll(function() { // Adiciona um evento de rolagem à janela
+      var scroll = $(window).scrollTop(); // Obtém a posição de rolagem atual
+
+      if (scroll > 100) { // Se a posição de rolagem for superior a 100 pixels
+        $(".navbar").addClass("scrolled"); // Adiciona a classe "scrolled" ao cabeçalho
+      } else {
+        $(".navbar").removeClass("scrolled"); // Remove a classe "scrolled" do cabeçalho
+      }
+    });
+  });
+  //ANIMAÇÕES
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      var windowHeight = $(window).height();
+      var scroll = $(window).scrollTop();
+
+      $('.section-hidden').each(function() {
+        var position = $(this).offset().top;
+
+        if (position < (scroll + windowHeight * 0.8)) {
+          $(this).addClass('section-visible');
+        }
+      });
+    });
+  });
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      var windowHeight = $(window).height();
+      var scroll = $(window).scrollTop();
+
+      $('.section-hidden-2').each(function() {
+        var position = $(this).offset().top;
+
+        if (position < (scroll + windowHeight * 0.8)) {
+          $(this).addClass('section-visible');
+        }
+      });
+    });
+  });
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      var windowHeight = $(window).height();
+      var scroll = $(window).scrollTop();
+
+
+      $('.section-hidden-3').each(function() {
+        var position = $(this).offset().top;
+
+        if (position < (scroll + windowHeight * 0.8)) {
+          $(this).addClass('section-visible');
+        }
+      });
+    });
+  });
